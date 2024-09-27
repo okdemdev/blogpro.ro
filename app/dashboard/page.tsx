46194a972252fd7e9c9d3986f9e58c7ebcc37e5c
Bulata Damian <inamida16@gmail.com>
2024-09-27 11:35:05 +0300

    fetching and rendering data on frontend

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,106 @@
-export default function DashboardIndexPage() {
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { EmptyState } from '../components/dashboard/EmptyState';
+import prisma from '../utils/db';
+import { requireUser } from '../utils/requireUser';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import DefaultImage from '@/public/default.png';
+
+async function getData(userId: string) {
+  const [sites, articles] = await Promise.all([
+    prisma.site.findMany({
+      where: {
+        userId: userId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 3,
+    }),
+    prisma.post.findMany({
+      where: {
+        userId: userId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 3,
+    }),
+  ]);
+
+  return { sites, articles };
+}
+
+export default async function DashboardIndexPage() {
+  const user = await requireUser();
+  const { articles, sites } = await getData(user.id);
   return (
     <div>
-      <h1>Hello world from dashboard!</h1>
+      <h1 className="text-2xl font-semibold mb-5">Your sites</h1>
+      {sites.length > 0 ? (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 lg:gap-7">
+          {sites.map((item) => (
+            <Card key={item.id}>
+              <Image
+                src={item.imageUrl ?? DefaultImage}
+                alt={item.name}
+                className="rounded-t-lg object-cover w-full h-[200px]"
+                width={400}
+                height={200}
+              />
+              <CardHeader>
+                <CardTitle className="truncate">{item.name}</CardTitle>
+                <CardDescription className="line-clamp-2">{item.description}</CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <Button asChild className="w-full">
+                  <Link href={`/dashboard/sites/${item.id}`}>View Articles</Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        <EmptyState
+          title="You dont have any sites created"
+          descripiton="You currently dont have any sites. Please create some so that you can see them right here"
+          href="/dashboard/sites/new"
+          buttonText="Create Site"
+        />
+      )}
+      <h1 className="text-2xl mt-10 font-semibold mb-5">Your Recent Articles</h1>
+      {articles.length > 0 ? (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 lg:gap-7">
+          {articles.map((item) => (
+            <Card key={item.id}>
+              <Image
+                src={item.image ?? DefaultImage}
+                alt={item.title}
+                className="rounded-t-lg object-cover w-full h-[200px]"
+                width={400}
+                height={200}
+              />
+              <CardHeader>
+                <CardTitle className="truncate">{item.title}</CardTitle>
+                <CardDescription className="line-clamp-2">{item.smallDescription}</CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <Button asChild className="w-full">
+                  <Link href={`/dashboard/sites/${item.siteId}/${item.id}`}>Edit Article</Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        <EmptyState
+          title="You dont have any articles created"
+          descripiton="You currently dont have any articles. Please create some so that you can see them right here"
+          href="/dashboard/sites"
+          buttonText="Create Article"
+        />
+      )}
     </div>
   );
 }
